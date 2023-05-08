fef54d716143c97c7a37599df0b7d2de6061f131
Vladimir RM <shvdrummer@gmail.com>
2023-05-08 11:38:53 +0300

    Add getstatic props

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "797a5317e815a548"; }
+/******/ 		__webpack_require__.h = function() { return "ddc038d8075057a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/users.js
+++ pages/users.js
@@ -31,3 +31,8 @@ const Users = () => {
 };
 
 export default Users;
+export async function getStaticProps(context) {
+    return {
+      props: {}, // will be passed to the page component as props
+    };
+  }
